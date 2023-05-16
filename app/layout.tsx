e15ee459f45980810850188f0f55bb07b694175f
Pawel Smolarski <smolenc1@gmail.com>
2023-05-16 14:39:15 +0200

    add UI Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,24 @@
-import './globals.css'
-import { Nunito } from 'next/font/google'
+import Navbar from "./components/Navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'ApartBooking',
-  description: 'Book the best aparts',
-}
+  title: "ApartBooking",
+  description: "Book the best aparts",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar/>
+        {children}</body>
     </html>
-  )
+  );
 }
