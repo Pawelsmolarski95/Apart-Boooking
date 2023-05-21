8ee678eb7426cae2e596544fad9fb0a29bcf4f10
Pawel Smolarski <smolenc1@gmail.com>
2023-05-21 20:51:50 +0200

    WIP rentModal

---
+++ app/components/Inputs/CategoryInput.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { IconType } from "react-icons";
+
+interface CategoryInputProps {
+  icon: IconType;
+  selected?: boolean;
+  label: string;
+  onClick: (value: string) => void;
+}
+
+const CategoryInput: React.FC<CategoryInputProps> = ({
+  icon: Icon,
+  selected,
+  label,
+  onClick,
+}) => {
+  return (
+    <div
+      onClick={() => onClick(label)}
+      className={`flex cursor-pointer flex-row gap-3  items-center rounded-xl 
+        border-2 p-4 transition hover:border-black 
+        ${
+          selected ? "border-black" : "border-neutral-200"
+        }   `}
+    >
+      <Icon size={30} />
+      <div className="font-semibold">{label}</div>
+    </div>
+  );
+};
+
+export default CategoryInput;
--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -1,18 +1,80 @@
-"use client"
+"use client";
 
-import React from "react";
+import React, { useMemo, useState } from "react";
 import Modal from "./Modal";
 import useRentModal from "@/app/hooks/useRentModal";
+import Heading from "../Heading";
+import { categories } from "../Navbar/Categories";
+import CategoryInput from "../Inputs/CategoryInput";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
   const rentModal = useRentModal();
+
+  const [step, setStep] = useState(STEPS.CATEGORY);
+
+  const onBack = () => {
+    setStep((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setStep((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+    return "Back";
+  }, [step]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8 ">
+      <Heading
+        title="Which of these best describies your place?"
+        subtitle="Pick a category"
+      />
+      <div className="grid max-h-[50vh] grid-cols-1 gap-3 overflow-y-auto md:grid-cols-2">
+        {categories.map((category) => (
+          <div key={category.label} className="col-span-1">
+            <CategoryInput
+              onClick={() => {}}
+              selected={false}
+              label={category.label}
+              icon={category.icon}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
   return (
     <Modal
       title="Airbnb your home"
       onClose={rentModal.onClose}
       isOpen={rentModal.isOpen}
       onSubmit={rentModal.onClose}
-      actionLabel="Submit"
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={
+        step === STEPS.CATEGORY ? undefined : onBack
+      }
+      body={bodyContent}
     />
   );
 };
