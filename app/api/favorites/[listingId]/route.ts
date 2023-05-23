8c5531740ba9ef3412d287c1e41149c42101c88a
Pawel Smolarski <smolenc1@gmail.com>
2023-05-23 17:48:21 +0200

    create POST for fav listings

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+interface IParmas {
+  listingId?: string;
+}
+
+export async function POST(
+  request: Request,
+  { params }: { params: IParmas }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== "string") {
+    throw new Error("Ivalid ID");
+  }
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds.push(listingId);
+
+  const user = await prisma.user.update({
+    where: {
+      id: currentUser.id,
+    },
+    data: { 
+        favoriteIds: favoriteIds
+    },
+  });
+
+  return NextResponse.json(user);
+}
