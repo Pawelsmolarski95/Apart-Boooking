c80d5ffe23adf0bfd1beca6d635695100e7abc3a
Pawel Smolarski <smolenc1@gmail.com>
2023-05-25 13:14:00 +0200

    fix problem with SafeListings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,18 +1,19 @@
-
 import prisma from "@/app/libs/prismadb";
 
-export default function getListings () {
+export default async function getListings() {
+  try {
+    const listings = await prisma.listing.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
 
-    try {
-        const listings = prisma.listing.findMany({
-            orderBy: {
-                createdAt: 'desc'
-            }
-        })
-        return listings;
-
-    } catch (error:any ) {
-        throw new Error(error)
-    }
-    
-} 
+    const safeListings = await listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }));
+    return safeListings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/components/Listings/ListingCard.tsx
+++ app/components/Listings/ListingCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { SafeUser } from "@/app/types";
+import { SafeListing, SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import React, { useCallback, useMemo } from "react";
 import useCountries from "@/app/hooks/useCountries";
@@ -12,7 +12,7 @@ import HeartButton from "../HeartButton";
 import Button from "../Button";
 
 interface ListingCardProps {
-  data: Listing;
+  data: SafeListing;
   reservation?: Reservation;
   onAction?: (id: string) => void;
   disabled?: boolean;
